2ba5f847fc6558ac51865d2dd007c8b1ba42cec5
aditikumari1710 <53184299+aditikumari1710@users.noreply.github.com>
2021-09-27 23:14:33 +0530

    Initial commit

---
+++ landing_script.js
@@ -0,0 +1,31 @@
+let alta =document.getElementsById('alta');
+let background=document.getElementsById("background");
+let title=document.getElementsById("title")
+
+function buttercup()
+{
+	alta.src="buttercup.jpg";
+    background.style.backgroundColor="#F2AA9B";
+    title.style.color="#F2AA9B";
+}
+function habibi()
+{
+	alta.src="buttercup.jpg";
+    background.style.backgroundColor=" #C2634E";
+    title.style.color=" #C2634E";
+}
+
+function amore()
+{
+	alta.src="amore.jpg";
+    background.style.backgroundColor="#773E32";
+    title.style.color="#773E32";
+}
+
+function babyface()
+{
+	alta.src="baby-face.jpg";
+    background.style.backgroundColor="#C67463";
+    title.style.color="#C67463";
+}
+
